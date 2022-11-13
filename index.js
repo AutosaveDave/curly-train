5f475d9c82026e0c44241fc5d1646dfcaff24d0f
David Coppock <106124456+dccoppock@users.noreply.github.com>
2022-11-12 20:58:35 -0500

    moved package.json, added index.js in main folder

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
+
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log("server working on port"+PORT+"!");
+  });
+});
